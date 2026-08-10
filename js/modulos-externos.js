71131e7fdc3b048fd8bb3049137da13e5476a08c
Andres Aparicio <35275130+andresapal@users.noreply.github.com>
2026-08-10 13:18:10 -0500

    Update modulos-externos.js

--- js/modulos-externos.js
+++ js/modulos-externos.js
@@ -8,8 +8,7 @@
  *  PARA CAMBIAR LA DIRECCION DEL MODULO, EDITA SOLO ESTA LINEA:
  * ─────────────────────────────────────────────────────────────
  */
-var GEOPROSPECTOR_URL = ''; // <- cuando este publicado, pon aqui la direccion
-                            //    ej: 'https://prospector.trasteosya.online'
+var GEOPROSPECTOR_URL = ''; 'https://geoprospector.vercel.app/prospeccion/nueva';
 
 (function () {
   'use strict';
